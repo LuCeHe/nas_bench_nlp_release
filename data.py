b6061b6d7ae686bc111abd7e4a5dcc6e35a95252
LuCeHe <manucelotti@gmail.com>
2020-12-11 22:49:00 +0100

    minor changes

--- data.py
+++ data.py
@@ -34,6 +34,7 @@ class Corpus(object):
     def tokenize(self, path):
         """Tokenizes a text file."""
         print(path)
+        print(os.listdir(os.path.split(path)[0]))
         assert os.path.exists(path)
         # Add words to the dictionary
         with open(path, 'r', encoding="utf8") as f:
